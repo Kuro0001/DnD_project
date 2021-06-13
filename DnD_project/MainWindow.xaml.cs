871d6cb52e2a790da532851f0ece808e55638f7d
Kuro0001 <antaa200@gmail.com>
2021-06-13 21:52:51 +0300

    Добавьте файлы проекта.

---
+++ DnD_project/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DnD_project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AddTab();
+        }
+
+        private void Aaaa_Notify(string message)
+        {
+            AddTab();
+        }
+
+        private void AddTab()
+        {
+
+            Grid grid = new Grid();
+            grid.RowDefinitions.Add(new RowDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+
+            UserControl_start new_tab_start = new UserControl_start();
+            new_tab_start.Notify += Aaaa_Notify;
+            ListBox notesList = new ListBox();
+            notesList.Items.Add("Macbook Pro");
+            notesList.Items.Add("HP Pavilion 5478");
+            notesList.Items.Add("Acer LK-08");
+
+            Grid.SetRow(new_tab_start, 0);
+            Grid.SetColumn(new_tab_start, 0);
+            grid.Children.Add(new_tab_start);
+
+            // добавление вкладки
+            Main_Tab.Items.Add(new TabItem
+            {
+                Header = new TextBlock { Text = "Начальная страница" }, // установка заголовка вкладки
+                Content = grid
+            });
+        }
+    }
+}
---
+++ DnD_project/UserControl_start.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DnD_project
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControl_start.xaml
+    /// </summary>
+    public partial class UserControl_start : UserControl
+    {
+        public delegate void EventHandler(string message);
+        public event EventHandler Notify;
+        
+        public delegate void AccountHandler(string message);
+        public event AccountHandler Notify2;
+        public UserControl_start()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty Choose_option = DependencyProperty.Register(
+    "Text", typeof(UserControl), typeof(UserControl_start), new PropertyMetadata(default(string)));
+
+        public UserControl Next_Option
+        {
+            get => (UserControl)GetValue(Choose_option);
+            set => SetValue(Choose_option, value);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //bubble the event up to the parent
+            Notify?.Invoke("dsadsa");
+            Notify2?.Invoke("dsadsa");
+        }
+    }
+}
